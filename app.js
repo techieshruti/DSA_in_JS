e010f0c67806da960b59d7b47be7750f0f1adee4
techieshruti <shrutichandra721@gmail.com>
2025-07-03 23:17:21 +0530

    move zero

--- app.js
+++ app.js
@@ -18,3 +18,22 @@ var twoSum = function(nums, target) {
 }
 
 console.log(twoSum);
+
+// 283. move zeros
+
+var moveZeroes = function(nums) {
+    let pos = 0;
+  for(let i=0; i<nums.length; i++)
+    {
+      if (nums[i] !== 0){
+      nums[pos] =nums[i];
+        pos++;
+      }
+    }
+ while (pos < nums.length) {
+  nums[pos] = 0;
+  pos++;
+}
+
+console.log(nums);
+};
