a05ca266038ffd2f8692d75a695808cb2b98874e
techieshruti <shrutichandra721@gmail.com>
2025-07-04 13:59:47 +0530

    Alternate elements

--- app.js
+++ app.js
@@ -36,4 +36,19 @@ var moveZeroes = function(nums) {
 }
 
 console.log(nums);
-};
+}
+
+// Alternate elements of an array
+let arr=[10,20,30,40,50,60,70,80,90,100];
+console.log(arr);
+let res = [];
+for(let i=0; i<arr.length; i++){
+    if(i % 2 == 0)
+    {
+        res.push(arr[i]);
+        
+    }
+}
+console.log(res);
+
+// for (let i = 0; i < arr.length; i += 2)
