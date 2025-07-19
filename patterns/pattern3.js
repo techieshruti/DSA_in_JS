91b344e6f3764da9c3ac40be2c647b4bf9fec399
techieshruti <shrutichandra721@gmail.com>
2025-07-20 00:31:25 +0530

    third pattern

--- patterns/pattern3.js
+++ patterns/pattern3.js
@@ -1,11 +1,10 @@
 // Pattern #3 where columns are increasing according to rows size and print numeric values
 // first loop is pointing to column and second loop is pointing rows.
 
-// step 1: create a loop which will go from 0 to i<=n
+// step 1: create a outer loop which will go from 0 to i<=n
 // step 2: create a variable to store result as we need to joint the stars. if we don't store it it will print on new line.
-// step 3: create a loop for rows it will go from 1 to j<=i
-// whatever the value of i, j will print its value according to i
-// step 4: add value of j to variable and store into same
+// step 3: create a inner loop for rows it will go from 1 to j<=i, whatever the value of i, inner loop will store it into variable
+// step 4: add value of i to variable and store into same
 // step 5: print the result of variable
 
 for(let i=0; i<=5; i++){
